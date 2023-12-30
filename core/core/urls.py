8f8797ea36df749835fd3a8f07826ccf7bc5171f
Mahdi-S97 <mahdisalimi.dev@gmail.com>
2023-12-30 21:25:05 +0330

    add api documentation

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,6 +17,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi 
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="api sample",
+        default_version='v1',
+        description="api sample",
+        terms_of_service="",
+        contact=openapi.Contact(email=""),
+        license=openapi.License(name=""),
+    ),
+    public=True,
+    permission_classes=(
+        permissions.AllowAny,
+    )
+    )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +43,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path("", include("todo.urls")),
     path('api-docs/', include_docs_urls(title='api sample')),
+    path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
